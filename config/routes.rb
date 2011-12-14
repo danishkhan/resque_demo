2afb252cca6163cc63896f5b60db842afa576fc4
Danish Khan <dankhan28@gmail.com>
2011-12-14 12:34:46 -0800

    removed index and welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ ResqueDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'eat#index'
 
   # See how all your routes lay out with "rake routes"
 
