aa3f269a85a4d978c4075f3cb76c1aa6118a5134
Danish Khan <dankhan28@gmail.com>
2011-12-16 16:57:03 -0800

    redis initializer

---
+++ config/initializers/redis.rb
@@ -0,0 +1,7 @@
+# Load the redis.yml configuration file
+redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env]
+
+# # Connect to Redis using the redis_config host and port
+if redis_config
+   $redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
+end
