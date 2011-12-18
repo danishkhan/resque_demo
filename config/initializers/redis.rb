76b7b438404c476e307ac54fa75fab3cf3c987c5
Danish Khan <dankhan28@gmail.com>
2011-12-17 16:42:36 -0800

    syntax testing

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,5 +4,5 @@ redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env]
 # Connect to Redis using the redis_config host and port
 if redis_config
 #   $redis = Redis.new(:host => redis_config['host'], :port => redis_config['port'])
-  $redis = Redis.new(:host => ip-10-168-78-158.us-west-1.compute.internal, :port => 6379)
+  $redis = Redis.new(:host => 'ip-10-168-78-158.us-west-1.compute.internal', :port => 6379)
 end
