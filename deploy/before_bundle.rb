748e4d8d1a53d52a8a2b47bd739571f3be0c9e57
Danish Khan <dankhan28@gmail.com>
2011-12-19 15:58:15 -0800

    changed to release path

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,2 +1,2 @@
 run "ln -nfs #{shared_path}/config/resque.yml #{current_path}/config/resque.yml"
-run "ln -nfs #{shared_path}/config/redis.yml #{current_path}/config/redis.yml"
+run "ln -nfs #{shared_path}/config/redis.yml #{release_path}/config/redis.yml"
